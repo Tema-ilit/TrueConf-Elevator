df9de1759638eb61f1378e326e839df90025bb4c
Tema-ilit <artem_eliseev_2015@mail.ru>
2024-05-01 19:22:48 +0300

    production

--- gitCommit.sh
+++ gitCommit.sh
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "production"
-git remote add origin https://github.com/Tema-ilit/TrueConf-Elevator.git
+git remote add origin "https://github.com/Tema-ilit/TrueConf-Elevator.git"
 git push -u origin main
